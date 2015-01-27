94ff5868a44fe212aa5420870bb47e5d2a502922
jessicafraines <jessicafraines@gmail.com>
2015-01-27 13:32:18 -0600

    fixed hamburger icon and made it animated

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,36 +1,5 @@
-(function() {
- 
-    // Create input element for testing
-    var input = document.createElement('input');
-     
-    // Create the supports object
-    var supports = {};
-     
-    supports.autofocus   = 'autofocus' in input;
-    supports.required    = 'required' in input;
-    supports.placeholder = 'placeholder' in input;
- 
-    // Fallback for autofocus attribute
-    if(!supports.autofocus) {
-         
-    }
-     
-    // Fallback for required attribute
-    if(!supports.required) {
-         
-    }
- 
-    // Fallback for placeholder attribute
-    if(!supports.placeholder) {
-         
-    }
-     
-    // Change text inside send button on submit
-    var send = document.getElementById('contact-submit');
-    if(send) {
-        send.onclick = function () {
-            this.innerHTML = '...Sending';
-        }
-    }
- 
-})();
+$(document).ready(function () {
+  $(".navbar-toggle").on("click", function () {
+    $(this).toggleClass("active");
+  });
+});
